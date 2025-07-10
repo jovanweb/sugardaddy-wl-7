97230b741b9f19581aba5098b1532d88baede23a
Jovanweb <jovanpoznanovic@gmail.com>
2025-07-10 15:41:37 +0200

    create project

---
+++ app.js
@@ -0,0 +1,51 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            currentStep: 6,
+			timeLeft: 300,
+			intervalId: null,
+			dropdown: false,
+			age: false,
+        }
+    },
+
+	computed: {
+		minutes() {
+		  return String(Math.floor(this.timeLeft / 60)).padStart(2, '0');
+		},
+		seconds() {
+		  return String(this.timeLeft % 60).padStart(2, '0');
+		},
+	},
+    methods: {
+
+        step(step) {
+			this.currentStep = step
+        },
+
+		ageValidation() {
+			this.age = true;
+		},
+
+		startCountdown() {
+			if (this.intervalId) clearInterval(this.intervalId);
+	  
+			this.intervalId = setInterval(() => {
+			  if (this.timeLeft > 0) {
+				this.timeLeft--;
+			  } else {
+				clearInterval(this.intervalId);
+				this.intervalId = null;
+			  }
+			}, 1000);
+		},
+    },
+    beforeUnmount() {
+		if (this.intervalId) clearInterval(this.intervalId);
+	},
+	mounted() {
+		this.startCountdown()
+	}
+}).mount('#app')
